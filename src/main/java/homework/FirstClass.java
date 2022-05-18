9fc5c60f3158beac4f068f6d7cb674cf96731309
sandis-elsts <104780818+sandis-elsts@users.noreply.github.com>
2022-05-18 22:10:03 +0300

    Added homework

--- src/main/java/homework/FirstClass.java
+++ src/main/java/homework/FirstClass.java
@@ -4,5 +4,44 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("this is homework");
 
+// Icaland Data
+        String countryName = "Iceland";
+        int population = 371580;
+        int area = 102775;
+        String capital = "Reykjavík";
+        String officialLanguage = "Icelandic";
+        boolean isEu = false;
+        String icelandCurrency = "Íkr";
+        char euCurrency = '€';
+
+//    Iceland text
+        System.out.println(countryName + " has " + population + " people " +
+                " and most of them are living in the Capital:" + capital + ", they speak " + officialLanguage +
+                " and use " + icelandCurrency + " as their currency, \nit would be easier if they would use " + euCurrency
+                + ". Are they members of EU? - " + isEu + "!");
+
+//        Aritmētiskie
+
+        int x = 2;
+        int y = 4;
+
+        System.out.println("Summa");
+        System.out.println(x+y);
+        System.out.println("Atņemšana");
+        System.out.println(x-y);
+        System.out.println("Reizināšana");
+        System.out.println(x*y);
+        System.out.println("Dalīšana");
+        System.out.println(x/y);
+        System.out.println("Atlikums");
+        System.out.println((x%y));
+        System.out.println("x+y+x-y+x*y+x/y+x%y=");
+        System.out.println(x+y+x-y+x*y+(x/y)+x%y);
+
+
+
+
+
+
     }
 }
